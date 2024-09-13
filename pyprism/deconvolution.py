dcc38c463c539e1cc3d8206b87b97c2073489247
Lukas Jonathan Haeuser <lukas.haeuser@empa.ch>
2024-09-13 16:36:33 +0200

    Hotfix for multi_parallel_deconvolution

--- pyprism/deconvolution.py
+++ pyprism/deconvolution.py
@@ -35,32 +35,28 @@ def multi_deconvolution(bulk_data: NormalFormRNASeqData,
                                number_of_iterations=number_of_iterations)
 
 
-@njit(parallel=True)
-def multi_parallel_deconvolution(bulk_data: NormalFormRNASeqData,
-                                 single_cell_reference: NormalFormRNASeqData,
-                                 number_of_iterations: PositiveInt
-                                 ) -> DeconvolutionResult:
+#@njit(parallel=True)
+def multi_parallel_deconvolution(bulk_data,
+                                 single_cell_reference,
+                                 number_of_iterations
+                                 ):
 
     # Normalize columns (cell types)
-    normalized_reference = single_cell_reference.X / single_cell_reference.X.sum(axis=0)
+    normalized_reference = single_cell_reference / single_cell_reference.sum(axis=0)
 
     # Initial guess (replace with  initial construction of expression_tensor)
-    cell_state_fraction = np.ones((single_cell_reference.n_vars, bulk_data.n_vars)) / single_cell_reference.n_vars
+    cell_state_fraction = np.ones((single_cell_reference.shape[1], bulk_data.shape[1])) / single_cell_reference.shape[1]
 
     # Iteration scheme
     for i in range(number_of_iterations):
         expression_tensor = normalized_reference[:, :, None] * cell_state_fraction[None, :, :]
         expression_tensor = expression_tensor / expression_tensor.sum(axis=1)[:, None, :]
-        expression_tensor = bulk_data.X[:, None, :] * expression_tensor
+        expression_tensor = bulk_data[:, None, :] * expression_tensor
         cell_state_fraction = expression_tensor.sum(axis=0)
         cell_state_fraction = cell_state_fraction / cell_state_fraction.sum(axis=0)
 
 
-    return DeconvolutionResult(expression_tensor=expression_tensor,
-                               gene_names=bulk_data.obs_names.to_numpy(),
-                               sample_names=bulk_data.var_names.to_numpy(),
-                               cell_state_names=single_cell_reference.var_names.to_numpy(),
-                               number_of_iterations=number_of_iterations)
+    return expression_tensor
 
 
 @beartype
